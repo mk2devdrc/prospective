25e67c1444d2ec04193578983815f72bf99ec04b
Mkwilfreid Mpunia <wilfreidmk@Mkwilfreids-MacBook-Pro.local>
2017-09-30 15:38:45 +0200

    french site done 90%

--- web/urls.py
+++ web/urls.py
@@ -1,8 +1,9 @@
 
 from django.conf.urls import url
 
-from .views import HomeView
+from .views import HomeView, NewsDetail
 
 urlpatterns = [
     url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^news/(?P<pk>\d+)/$', NewsDetail.as_view(), name='news-detail'),
 ]
--- web/views.py
+++ web/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.views import generic
 
+from web import models
+
 # Create your views here.
 class HomeView(generic.TemplateView):
     template_name = 'web/french/index.html'
+
+
+class NewsDetail(generic.DetailView):
+    model = models.Francais
+    template_name = 'web/french/detailed_news.html'
